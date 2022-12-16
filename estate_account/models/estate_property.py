064a3eaf1f4db3a90339a88e978fd3c327a2a784
LinhThuy <ntthuylinh1998@gmail.com>
2022-12-16 15:15:13 +0700

    Edit security rule so that user can create property, and can see only their own invoice

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -26,6 +26,7 @@ class EstateProperty(models.Model):
             {'name': 'Administrative fees', 'quantity': 1, 'price_unit': 100.00, 'account_id': account_id}
         ]
         invoice_vals = {
+            'company_id': self.env.user.company_id.id,
             'partner_id': self.buyer_id.id,
             'user_id': self.salesman_id.id,
             'move_type': 'out_invoice',
